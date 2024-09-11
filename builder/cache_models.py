2de41eec07d6c054f8c545608625f77d6fe16d4f
Angel Rodriguez Junquera <angel@sublimestudio.ch>
2024-09-11 14:48:03 +0200

    Cache models

--- builder/cache_models.py
+++ builder/cache_models.py
@@ -1,7 +1,7 @@
 # builder/model_fetcher.py
 
 import torch
-from diffusers import StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline, AutoencoderKL
+from diffusers import StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline, AutoencoderKL, ControlNetModel, CLIPVisionModelWithProjection
 
 
 def fetch_pretrained_model(model_class, model_name, **kwargs):
@@ -21,25 +21,47 @@ def fetch_pretrained_model(model_class, model_name, **kwargs):
 
 
 def get_diffusion_pipelines():
+    
     '''
     Fetches the Stable Diffusion XL pipelines from the HuggingFace model hub.
     '''
+    !apt-get install git-lfs
+
     common_args = {
         "torch_dtype": torch.float16,
         "variant": "fp16",
         "use_safetensors": True
     }
 
-    pipe = fetch_pretrained_model(StableDiffusionXLPipeline,
-                                  "stabilityai/stable-diffusion-xl-base-1.0", **common_args)
-    # not needed for now. We might want to cache this in the future and the CSGO models              
-    # vae = fetch_pretrained_model(
-    #     AutoencoderKL, "madebyollin/sdxl-vae-fp16-fix", **{"torch_dtype": torch.float16}
-    # )
-    # print("Loaded VAE")
-    # refiner = fetch_pretrained_model(StableDiffusionXLImg2ImgPipeline,
-    #                                  "stabilityai/stable-diffusion-xl-refiner-1.0", **common_args)
+    # CSGO models
 
+    #base
+    fetch_pretrained_model(StableDiffusionXLPipeline,
+                                  "stabilityai/stable-diffusion-xl-base-1.0", **common_args)
+
+    #vae
+    fetch_pretrained_model(AutoencoderKL,
+                                  "madebyollin/sdxl-vae-fp16-fix", **common_args)
+
+    #control net
+    !git clone https://huggingface.co/TTPlanet/TTPLanet_SDXL_Controlnet_Tile_Realistic
+    !mv TTPLanet_SDXL_Controlnet_Tile_Realistic/TTPLANET_Controlnet_Tile_realistic_v2_fp16.safetensors TTPLanet_SDXL_Controlnet_Tile_Realistic/diffusion_pytorch_model.safetensors
+
+    fetch_pretrained_model(ControlNetModel,
+                                  "./TTPLanet_SDXL_Controlnet_Tile_Realistic/", **common_args)
+    
+
+    #ip adapter
+    !git clone https://huggingface.co/InstantX/CSGO
+
+    
+    #image_encoder
+    CLIPVisionModelWithProjection.from_pretrained("h94/IP-Adapter", subfolder="sdxl_models/image_encoder")
+
+    #Check if needed
+    BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+    BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+  
     return pipe
 
 
